d99bfd0b3727f87aee7b24ad77bb18249bda7e7a
kurtzxz <131712632+kurtzxz@users.noreply.github.com>
2023-04-24 17:01:43 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+num1 = prompt('Digite o primeiro valor: ')
+num2 = prompt('Digite o segundo valor: ')
+soma = parseInt(num1)+parseInt(num2)
+window.alert("A soma é: " + soma)
+
+
+// parseInt transforma a variavel para inteiro
+// window.alert = janela de aviso que aparece no topo do site, só serve para output
